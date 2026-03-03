9f1b0388b92eed1872d28c2e538e689a1bf618f9
Sesadesigns <olesyakipriyanova@outlook.com>
2026-03-03 10:58:58 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,70 @@
+(document.addEventListener("DOMContentLoaded", () => {
+
+const navLinks = document.querySelectorAll(".nav-horizontal a"); 
+const artists = document.querySelectorAll(".artist"); 
+const artworks = document.querySelectorAll(".artwork"); 
+const allDetails = document.querySelectorAll(".artist details");
+    
+    navLinks.forEach(link => { 
+        link.addEventListener("click", e => { 
+            e.preventDefault();
+
+    const selectedMovement = link.dataset.movements.toLowerCase();
+       
+    artworks.forEach(img => { 
+        const movements = img.dataset.movement 
+            .toLowerCase() 
+            .split(" ");
+            
+        img.style.display = 
+        movements.includes(selectedMovement) ? "block" : "none"; });
+        
+        artists.forEach(artist => { const movements = artist.dataset.movement .toLowerCase() .split(" ");
+            if (movements.includes(selectedMovement)) { 
+                artist.style.display = "block"; 
+            } else { 
+                artist.style.display = "none";
+            } 
+        });
+
+            const selectedDiscipline = link.dataset.disciplines.toLowerCase();
+                
+        
+            artworks.forEach(img => { 
+                const disciplines = img.dataset.discipline 
+                .toLowerCase() 
+                .split(" ");
+
+            img.style.display = disciplines.includes(selectedDiscipline) 
+                ? "block" : "none"; 
+            });
+            
+            artists.forEach(artist => {        
+                const movements = artist.dataset.movement 
+                .toLowerCase() .split(" ");
+                    
+                if (movements.includes(selectedMovement)) { 
+                    artist.style.display = "block"; 
+                } else { 
+                    artist.style.display = "none"; 
+                } 
+                });
+
+            allDetails.forEach(details => { 
+                details.open = false; 
+            }); 
+        }); 
+    });
+            allDetails.forEach(details => { 
+                details.addEventListener("toggle", () => { 
+                    if (details.open) { 
+                        allDetails.forEach(other => { 
+                    if (other !== details) { 
+                        other.open = false; 
+                    } 
+                }); 
+            } 
+        }); 
+    }); 
+})); 
+artworks.forEach(img => details.open = false);
